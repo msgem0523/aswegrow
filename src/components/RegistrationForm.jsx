eb8dc00a41fd351fe70c58a41d1c52dd68e04c8e
tanewton0523 <59624833+tanewton0523@users.noreply.github.com>
2025-05-28 19:50:49 -0400

    initialization, create component folder, create registration form

---
+++ src/components/RegistrationForm.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+const RegistrationForm = () => {
+    const [formData, setFormData] = useState({
+        parentFirstName: "",
+        parentLastName: "",
+        email: "",
+        phone: "",
+        childName: "",
+        childAge: "", //update to dob and add age calculation
+        datePlanted: "",
+        waiverAccepted: false,
+    });
+
+    const handleChange = (e) => {
+        const { name, value, type, checked } = e.target;
+        const newValue = type === "checkbox" ? checked : value;
+        setFormData({ ...formData, [name]: newvalue });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Submitted data:", formData);
+        // send to backend
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>🌱 As We Grow: Event Registration</h2>
+            <label>Parent First Name:
+                <input type="text" name="parentFirstName" onChange={handleChange} required />
+            </label><br />
+            <label>Parent Last Name:
+                <input type="text" name="parentLastName" onChange={handleChange} required />
+            </label><br />
+            <label>Email:
+                <input type="email" name="email" onChange={handleChange} required />
+            </label><br />
+            <label>Phone:
+                <input type="tel" name="phone" onChange={handleChange} required />
+            </label><br />
+            <label>Child's Name:
+                <input type="text" name="childName" onChange={handleChange} required />
+            </label><br />
+            <label>Child's Age:
+                <input type="number" name="childAge" onChange={handleChange} required />
+            </label><br />
+            <label>Date Planted:
+                <input type="date" name="datePlanted" onChange={handleChange} required />
+            </label><br />
+            <label>Photo Waiver:
+                <input type="checkbox" name="waiverAccepted" onChange={handleChange} required />
+            </label><br />
+            <button type="submit">Submit</button>
+        </form>
+    );
+};
+
+export default RegistrationForm;
