7a9a9e937540af857eb1046e6d73fc60d8104dc0
tanewton0523 <59624833+tanewton0523@users.noreply.github.com>
2025-05-30 22:29:42 -0400

    created style page

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Waiver from "./components/Waiver.jsx";
+import "./styles.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
